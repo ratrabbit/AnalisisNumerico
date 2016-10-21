c77262e65356d9698224a21a6abbe14464153c89
ratrabbit <ratrabbit2@hotmail.com>
2016-10-20 20:21:02 -0500

    7.1.3 terminado

--- Practica2/Practica2_ejercicio713.py
+++ Practica2/Practica2_ejercicio713.py
@@ -14,24 +14,29 @@ def naiveGauss(n,a,b,x):
             for j in range(k+1,n):
                 a[i][j]-=xmult*a[k][j]
             b[i] -= xmult*b[k]
-    x[n] = b[n]/a[n][n]
+    x[n-1] = b[n-1]/a[n-1][n-1]
     for i in range(n-2,-1,-1):
         suma = b[i]
         for j in range(i+1,n):
-            suma -= a[i][j]*x[j]
+            suma += a[i][j]*x[j]
         x[i] = suma/a[i][i]
-        
+
 n = 2
 matrizA = np.zeros(shape=(n,n))
 for i in range(0,n):
     for j in range(0,n):
         matrizA[i][j]=i+j
+print matrizA
 matrizB = np.zeros(n)
 for i in range(0,n):
     matrizB[i]=i+1
+print matrizB
 matrizX = np.zeros(n)
 naiveGauss(n,matrizA,matrizB,matrizX)
+print matrizA
+print matrizX
 print matrizB
+
 #Resolver usando naive Gauss
 
         
